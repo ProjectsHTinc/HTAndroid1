b650793ae6acf5f857f383710f77c886f6bf6ba3
Nandha <nandha@happysanztech.com>
2022-01-21 15:44:32 +0530

    test

--- app/src/main/java/com/hst/osa_tatva/activity/SplashscreenActivity.java
+++ app/src/main/java/com/hst/osa_tatva/activity/SplashscreenActivity.java
@@ -25,6 +25,7 @@ public class SplashscreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//
 
         setContentView(R.layout.activity_splashscreen);
         database = new SQLiteHelper(getApplicationContext());
